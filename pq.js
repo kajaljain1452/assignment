88dce30aba7ec782d7922ccbf5542686102c1263
kajaljain1 <kajaljain145>
2018-06-10 21:23:24 +0530

    filesadded

---
+++ pq.js
@@ -0,0 +1,74 @@
+/*var a = 1
+var b = 2
+console.log('a:'+a,'b:'+b )
+
+
+function mul(a,b)
+{
+    return a*b
+}
+    console.log(mul(1,2))
+
+
+var a ='kajal'
+var b ='jain'
+console.log(a+b)
+console.log(a.length,b.length)
+
+
+var str = "Please locate where 'locate' occurs!"
+console.log(str.indexOf('where'),str.lastIndexOf("locate"))
+
+
+var str = "Please locate where 'locate' occurs!"
+console.log(str.slice(7,13))
+
+
+str = "Please visit Microsoft!"
+console.log(str.replace("Microsoft!","google"))
+
+
+var text1 = "Hello World!"
+console.log(text1.toLowerCase())
+console.log(text1.toUpperCase())
+
+
+var text = "Hello!"
+console.log(text.split('')+' ' +typeof text.split())  //convert string to array
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"]
+//fruits.push("Pear")
+//fruits.pop("Apple")
+//console.log(fruits)
+//console.log(fruits.join("")) //covert array to string)
+//fruits.splice(1,1,'hello')
+fruits[2]='hey'
+console.log(fruits)*/
+
+
+/*var points = [40, 100, 1, 5, 25, 10]
+console.log(points.sort(points)) */ //sort the given array
+
+
+
+/*function randomNumbers()
+{
+    for(let i=0;i<100;i++)
+{
+    console.log(parseInt(Math.random()*100))
+}
+
+} randomNumbers()*/
+
+
+
+ /*var x = 9.656
+ console.log(typeof x)
+ //console.log(x.toString(x))  // convert number to string
+ //console.log(typeof x.toString())
+ console.log(x.toFixed(2))
+
+
+ var x = 7.65
+ console.log(Math.sqrt(x)) */ //to find square root of a number
